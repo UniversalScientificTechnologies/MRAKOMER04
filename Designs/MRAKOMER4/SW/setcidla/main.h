53b9ac6883dc41323f773df676f5d0246b45982d
kakl <kakl@f55caa52-0715-0410-8694-db4083128028>
2009-07-08 22:17:25 +0000

    Pridany zdrojaky nastaveni adresi Thermopile senzoru do MM.

---
+++ Designs/MRAKOMER4/SW/setcidla/main.h
@@ -0,0 +1,19 @@
+#include <16F88.h>
+#device adc=8
+
+#FUSES NOWDT                 	//Watch Dog Timer
+#FUSES INTRC_IO              	//Internal RC Osc, no CLKOUT
+#FUSES PUT                 	//Power Up Timer
+#FUSES NOMCLR                	//Master Clear pin used for I/O
+#FUSES NOBROWNOUT            	//No brownout reset
+#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOCPD                 	//No EE protection
+#FUSES NOWRT                 	//Program memory not write protected
+#FUSES NODEBUG               	//No Debug mode for ICD
+#FUSES NOPROTECT             	//Code not protected from reading
+#FUSES NOFCMEN               	//Fail-safe clock monitor disabled
+#FUSES NOIESO                	//Internal External Switch Over mode disabled
+
+#use delay(clock=4000000)
+#use rs232(baud=2400,parity=N,xmit=PIN_B5,rcv=PIN_B2,bits=8)//,FORCE_SW)
+#use i2c(MASTER,SLOW,sda=PIN_B1,scl=PIN_B4,SMBUS)
