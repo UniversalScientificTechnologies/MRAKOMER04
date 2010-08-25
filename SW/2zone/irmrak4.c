8399bc8c1da7967c28259464a13f5e54cdcd4834
KAKL <kakl@users.noreply.github.com>
2010-08-25 19:47:58 +0000

    Dvouzonova verze.

---
+++ SW/2zone/irmrak4.c
@@ -0,0 +1,252 @@
+/**** IR Mrakomer 4 ****/
+#define VERSION "4.1"
+#define ID "$Id$"
+
+#include "irmrak4.h"
+
+#bit CREN = 0x18.4      // USART registers
+#bit SPEN = 0x18.7
+#bit OERR = 0x18.1
+#bit FERR = 0x18.2
+
+#include <string.h>
+
+#CASE    // Case sensitive compiler
+
+#define  MAXHEAT        20       // Number of cycles for heating
+#define  MAXOPEN        20       // Number of cycles for dome open
+#define  MEASURE_DELAY  6000     // Delay to a next measurement
+#define  RESPONSE_DELAY 100      // Reaction time after receiving a command
+#define  SAFETY_COUNT   90       // Time of one emergency cycle
+#define  SEND_DELAY     50       // Time between two characters on RS232
+
+#define  DOME        PIN_B4   // Dome controll port
+#define  HEATING     PIN_B3   // Heating for defrosting
+
+
+char  VER[4]=VERSION;   // Buffer for concatenate of a version string
+
+int8  heat;    // Status variables
+int8  open;
+
+inline void toggle_dome(void)    // Wire exercise
+{
+   if (open>0)
+   {output_toggle(DOME);}  // Toggle = Open Dome
+   else
+   {output_high(DOME);}    // Do not toggle = Close Dome
+}
+
+void delay(int16 cycles)         // Wire exercise with delay
+{
+   int16 i;
+
+   for(i=0; i<cycles; i++) {toggle_dome(); delay_us(100);}
+}
+
+void welcome(void)               // Welcome message
+{
+   char  REV[50]=ID;       // Buffer for concatenate of a version string
+
+   if (REV[strlen(REV)-1]=='$') REV[strlen(REV)-1]=0;
+   printf("\n\r\n\r# Mrakomer %s (C) 2007 KAKL\n\r",VER);   // Welcome message
+   printf("#%s\n\r",&REV[4]);
+   printf("#\n\r");
+   printf("# h - Switch On Heating for 20s.\n\r");
+   printf("# c - Need Colder. Switch Off Heating.\n\r");
+   printf("# o - Open the Dome for 20s.\n\r");
+   printf("# l - Lock the Dome.\n\r");
+   printf("# x - Open the Dome and switch On Heating.\n\r");
+   printf("# i - Print this Information.\n\r");
+   printf("# r - Repeat measure every second.\n\r");
+   printf("# s - Single measure.\n\r");
+   printf("# u - Update firmware. Go to the Boot Loader.\n\r");
+   printf("#\n\r");
+   printf("# <sequence> <ambient[1/100 C]> <ambient[1/100 C]> <sky[1/100 C]> ");
+   printf("<heating[s]> <dome[s]> <check>\n\r\n\r");
+//---WDT
+   restart_wdt();
+}
+
+
+#include "smb.c"                 // System Management Bus driver
+
+
+// Read sensor's RAM
+// Returns temperature in °K
+int16 ReadTemp(int8 addr, int8 select)
+{
+   unsigned char arr[6];         // Buffer for the sent bytes
+   int8 crc;                     // Readed CRC
+   int16 temp;                   // Readed temperature
+
+   addr<<=1;
+
+   SMB_STOP_bit();               //If slave send NACK stop comunication
+   SMB_START_bit();              //Start condition
+   SMB_TX_byte(addr);
+   SMB_TX_byte(RAM_Access|select);
+   SMB_START_bit();              //Repeated Start condition
+   SMB_TX_byte(addr);
+   arr[2]=SMB_RX_byte(ACK);      //Read low data,master must send ACK
+   arr[1]=SMB_RX_byte(ACK);      //Read high data,master must send ACK
+   temp=make16(arr[1],arr[2]);
+   crc=SMB_RX_byte(NACK);        //Read PEC byte, master must send NACK
+   SMB_STOP_bit();               //Stop condition
+
+   arr[5]=addr;
+   arr[4]=RAM_Access|select;
+   arr[3]=addr;
+   arr[0]=0;
+   if (crc != PEC_calculation(arr)) temp=0; // Calculate and check CRC
+
+   return temp;
+}
+
+
+/*-------------------------------- MAIN --------------------------------------*/
+void main()
+{
+   unsigned int16 seq, temp, tempa;
+   signed int16 ta, to1, to2;
+   int8 safety_counter;
+   int1 repeat;
+
+   output_high(DOME);                   // Close Dome
+   output_low(HEATING);                 // Heating off
+
+   delay_ms(1000);
+   restart_wdt();
+
+   seq=0;         // Variables initiation
+   heat=0;
+   open=0;
+   repeat=TRUE;
+
+   welcome();
+
+   tempa=ReadTemp(SA, RAM_Tamb);       // Dummy read
+   temp=ReadTemp(SA, RAM_Tobj1);
+
+   delay_ms(1000);
+//---WDT
+   restart_wdt();
+
+   while(TRUE)    // Main Loop
+   {
+      safety_counter=SAFETY_COUNT;  // Heating and Dome  Count Down
+      do
+      {
+         if (safety_counter<SAFETY_COUNT) safety_counter++;
+
+         delay(RESPONSE_DELAY);
+
+         if (safety_counter>=SAFETY_COUNT)
+         {
+            if (heat>0) heat--;
+            if (open>0) open--;
+
+            if (heat>0) { output_high(HEATING); } else { output_low(HEATING); }
+
+            safety_counter=0;
+//---WDT
+            restart_wdt();
+         }
+      } while (!kbhit()&&!repeat);
+
+//---WDT
+      restart_wdt();
+      {                 // Retrieve command
+         char ch='k';
+
+         if(kbhit()) ch=getc();
+
+         switch (ch)
+         {
+            case 'h':
+               heat=MAXHEAT;           // Need heating
+               break;
+
+            case 'c':
+               heat=0;                 // Need colder
+               break;
+
+            case 'o':
+               open=MAXOPEN;           // Open the dome
+               break;
+
+            case 'x':
+               open=MAXOPEN;           // Open the dome
+               heat=MAXHEAT;           // Need heating
+               break;
+
+            case 'l':
+               open=0;                 // Lock the dome
+               break;
+
+            case 'i':
+               if (open==0) welcome(); // Information about version, etc...
+               break;                  // Only when dome is closed
+
+            case 'r':
+               repeat=TRUE;            // Repeated measure mode
+               break;
+
+            case 's':
+               repeat=FALSE;            // Single measure mode
+               break;
+
+            case 'u':
+               reset_cpu();             // Update firmware
+         }
+      }
+//      while(kbhit()) getc();        // Flush USART buffer
+      CREN=0; CREN=1;               // Reinitialise USART
+
+      seq++;        // Increment the number of measurement
+
+      tempa=ReadTemp(SA, RAM_Tamb);       // Read temperatures from sensor
+      ta=tempa*2-27315;    // °K -> °C
+
+      temp=ReadTemp(SA, RAM_Tobj1);
+      to1=temp*2-27315;
+      temp=ReadTemp(SA, RAM_Tobj2);
+      to2=temp*2-27315;
+
+      { // printf
+         char output[8];   // Output buffer
+         int8 j;           // String pointer
+         int8 check=0;     // Checksum is calculated between '$' and '*'
+
+         delay(SEND_DELAY);
+         putc('$');
+         delay(SEND_DELAY);
+         sprintf(output,"M%s ",VER);
+         j=0; while(output[j]!=0) { delay(SEND_DELAY); putc(output[j]); check^=output[j++]; }
+         sprintf(output,"%Lu ", seq);
+         j=0; while(output[j]!=0) { delay(SEND_DELAY); putc(output[j]); check^=output[j++]; }
+         sprintf(output,"%Ld ", ta);
+         j=0; while(output[j]!=0) { delay(SEND_DELAY); putc(output[j]); check^=output[j++]; }
+         sprintf(output,"%Ld ", to1);
+         j=0; while(output[j]!=0) { delay(SEND_DELAY); putc(output[j]); check^=output[j++]; }
+         sprintf(output,"%Ld ", to2);
+         j=0; while(output[j]!=0) { delay(SEND_DELAY); putc(output[j]); check^=output[j++]; }
+         sprintf(output,"%u ", heat);
+         j=0; while(output[j]!=0) { delay(SEND_DELAY); putc(output[j]); check^=output[j++]; }
+         sprintf(output,"%u ", open);
+         j=0; while(output[j]!=0) { delay(SEND_DELAY); putc(output[j]); check^=output[j++]; }
+         sprintf(output,"*%X\n\r\0", check);
+         j=0; while(output[j]!=0) { delay(SEND_DELAY); putc(output[j++]); }
+         delay(SEND_DELAY);
+      }
+
+//---WDT
+      restart_wdt();
+      delay(MEASURE_DELAY);   // Delay to a next measurement
+//---WDT
+      restart_wdt();
+   }
+}
+
+
+#include "dbloader.c" // Space reservation for the BootLoader
--- SW/setcidla/main.h
+++ SW/setcidla/main.h
@@ -1,19 +1,19 @@
-#include <16F88.h>
+#include <16F877A.h>
 #device adc=8
 
-#FUSES NOWDT                 	//Watch Dog Timer
-#FUSES INTRC_IO              	//Internal RC Osc, no CLKOUT
-#FUSES PUT                 	//Power Up Timer
-#FUSES NOMCLR                	//Master Clear pin used for I/O
-#FUSES NOBROWNOUT            	//No brownout reset
-#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
-#FUSES NOCPD                 	//No EE protection
-#FUSES NOWRT                 	//Program memory not write protected
-#FUSES NODEBUG               	//No Debug mode for ICD
-#FUSES NOPROTECT             	//Code not protected from reading
-#FUSES NOFCMEN               	//Fail-safe clock monitor disabled
-#FUSES NOIESO                	//Internal External Switch Over mode disabled
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES HS                       //High speed Osc (> 4mhz)
+#FUSES NOPUT                    //No Power Up Timer
+#FUSES NOPROTECT                //Code not protected from reading
+#FUSES NODEBUG                  //No Debug mode for ICD
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOCPD                    //No EE protection
+#FUSES NOWRT                    //Program memory not write protected
 
-#use delay(clock=4000000)
-#use rs232(baud=2400,parity=N,xmit=PIN_B5,rcv=PIN_B2,bits=8)//,FORCE_SW)
-#use i2c(MASTER,SLOW,sda=PIN_B1,scl=PIN_B4,SMBUS)
+#use delay(clock=20000000) // 14318180)
+//#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8) // Pro USART
+#use rs232(baud=9600,parity=N,bits=8,xmit=PIN_B7,rcv=PIN_B6) // Pro PICkit2
+
+#use i2c(Master,Fast,sda=PIN_C4,scl=PIN_C3,SMBUS)
+
