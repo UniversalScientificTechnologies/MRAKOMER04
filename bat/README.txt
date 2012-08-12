c9e55453cd2f760c19dc73c49948ce53f69fabca
Jakub Kákona <kaklik@mlab.cz>
2012-08-12 21:25:00 +0000

    aktualizace skriptu a vylepseni dokumentace.

---
+++ bat/README.txt
@@ -0,0 +1,4 @@
+h.sh -- heating activation
+logger.sh -- simple logging script 
+plotMM.plt -- gnuplot data plot script
+uf.sh -- firmware upload script
--- bat/uf.sh
+++ bat/uf.sh
@@ -1,4 +1,3 @@
-4# $1 = /dev/ttyUSB0 for MM4.0
 echo /dev/ttyUSB$1
 echo uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu > /dev/ttyUSB$1
 echo u
@@ -6,4 +5,4 @@ sleep 5
 echo uf > /dev/ttyUSB$1
 echo uf
 sleep 5
-ascii-xfr -s -v -c1 ./irmrak4.hex > /dev/ttyUSB$1
+ascii-xfr -s -v -c1 ../SW/HEX/irmrak4.hex > /dev/ttyUSB$1
